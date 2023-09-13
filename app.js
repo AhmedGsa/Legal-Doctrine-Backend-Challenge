2018f534f64cdef33d042c79af91f5dd85433c38
SilverBullet19 <a_gouasmia@estin.dz>
2023-09-14 00:17:09 +0100

    setup swagger ui

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const connectDB = require('./db/connect');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const cors = require('cors');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // routers
 
@@ -15,6 +19,8 @@ const port = 3000 || process.env.PORT;
 
 // middleware
 app.use(express.json());
+app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/purchases', purchaseRouter);
